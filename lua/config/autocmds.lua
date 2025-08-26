c3ad7029bc4e8aaa60e790825fcf22cd1bcef8ee
Caio Lesnock <caio.dev@outlook.com>
2025-08-26 13:58:46 -0300

    Lazyvim install + php config

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+--Copy to clipboard
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has "wsl" == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+    callback = function() vim.fn.system("clip.exe", vim.fn.getreg '"') end,
+  })
+end
